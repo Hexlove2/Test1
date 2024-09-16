42086f7f21fe354f3e94a70d723859fff5c1cb99
Hexlove2 <hexlove12@gmail.com>
2024-09-16 11:43:49 +0800

    Add some class

--- src/test1.cpp
+++ src/test1.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
+class person {
+public:
+  person(int age1, string name1) {
+    name = name1;
+    age = age1;
+  }
+  string name;
+  int age;
+};
+
 int main(int argc, char *argv[]) {
   string newtest;
+  person person1(20, "项云辉");
+  cout << "名字：" << person1.name << endl << "年龄: " << person1.age << endl;
   return 0;
 }
